ffab895a840ba8e33a181ca78df46a7617d59a26
Szymon Nowak <szymon.nowak@allegro.pl>
2020-05-17 09:49:18 +0200

    some change

--- src/main/java/com/sda/singleton/CountAction.java
+++ src/main/java/com/sda/singleton/CountAction.java
@@ -4,7 +4,7 @@ public class CountAction implements Runnable {
 
     @Override
     public void run() {
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 1000000; i++) {
             CountService.instanceOf().increment();
         }
     }
