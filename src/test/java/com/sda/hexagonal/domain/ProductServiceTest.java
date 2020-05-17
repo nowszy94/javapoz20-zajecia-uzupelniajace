d910ea9db134d7a7069ede242b478fa15a6d03ca
Szymon Nowak <szymon.nowak@allegro.pl>
2020-05-17 13:31:54 +0200

    hexagonal #3 - additional test for exception thrown

--- src/test/java/com/sda/hexagonal/domain/ProductServiceTest.java
+++ src/test/java/com/sda/hexagonal/domain/ProductServiceTest.java
@@ -23,4 +23,18 @@ public class ProductServiceTest {
 
         Assert.assertEquals("Product is different", expectedProduct, actualProduct);
     }
+
+    @Test(expected = ProductAlreadyExistsException.class)
+    public void shouldThrowProductAlreadyExistsExceptionWhileTryingToCreateProductWithSameName() {
+        // given
+        ProductService productService = new ProductService(new FakeProductRepository());
+
+        String name = "product-2";
+        String description = "product-description";
+        ProductCategory category = ProductCategory.PHONE;
+        Double weight = 1.5;
+
+        // when
+        productService.addProduct(name, description, category, weight);
+    }
 }
