5ced309b7e28fdb17207180ec264687a8e877e99
Szymon Nowak <szymon.nowak@allegro.pl>
2020-05-17 12:48:29 +0200

    hexagonal #2 - testing addProduct method with junit and fake repository

--- src/main/java/com/sda/hexagonal/domain/Product.java
+++ src/main/java/com/sda/hexagonal/domain/Product.java
@@ -1,8 +1,10 @@
 package com.sda.hexagonal.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Product {
     private Long id;
     private String name;
---
+++ src/test/java/com/sda/hexagonal/domain/ProductServiceTest.java
@@ -0,0 +1,26 @@
+package com.sda.hexagonal.domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ProductServiceTest {
+
+    @Test
+    public void shouldCreateProduct() {
+        // given
+        ProductService productService = new ProductService(new FakeProductRepository());
+
+        String name = "product-name";
+        String description = "product-description";
+        ProductCategory category = ProductCategory.PHONE;
+        Double weight = 1.5;
+
+        // when
+        Product actualProduct = productService.addProduct(name, description, category, weight);
+
+        // then
+        Product expectedProduct = new Product(1L, name, description, category, weight);
+
+        Assert.assertEquals("Product is different", expectedProduct, actualProduct);
+    }
+}
